a9a6d97490ca7122c0bd74fefb36b2139c7a3f41
Ale Pereira <naitwa-alexandre@gmail.com>
2024-11-20 10:20:15 -0300

    add controller logic to implement commands

--- src/constants/directions.ts
+++ src/constants/directions.ts
@@ -3,4 +3,8 @@ export const EAST = 'E';
 export const SOUTH = 'S';
 export const WEST = 'W';
 
+export const LEFT = 'L';
+export const RIGHT = 'R';
+export const MOVE = 'M';
+
 export const directions = [NORTH, EAST, SOUTH, WEST] as const;
---
+++ src/controllers/roverCotnroller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { Position, RoverCommand } from "../types/typtes";
+import { moveForward, turnLeft, turnRight } from "../services/roverService";
+import { LEFT, MOVE, RIGHT } from "../constants/directions";
+
+export function processRoverCommands(req: Request, res: Response): void {
+  const { position, commands }: RoverCommand = req.body;
+  let currentPosition: Position = position;
+
+  commands.split('').forEach(command => {
+    command = command.toUpperCase();
+
+    if (command === LEFT) currentPosition.direction = turnLeft(currentPosition.direction);
+    if (command === RIGHT) currentPosition.direction = turnRight(currentPosition.direction);
+    if (command === MOVE) currentPosition = moveForward(currentPosition);
+  });
+
+  res.json({ finalPosition: currentPosition });
+}
